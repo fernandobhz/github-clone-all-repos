e07dc1c91c0bad18ee3dd4d36e59d87286f85a1f
Fernando Reis Guimarães <fernandobhz@gmail.com>
2024-04-04 23:30:49 -0300

    feat: github clone all repos

---
+++ README.md
@@ -0,0 +1,1 @@
+"# github-clone-all-repos" 
---
+++ index.js
@@ -0,0 +1,38 @@
+const { Octokit } = require("@octokit/rest");
+const { execSync } = require("child_process");
+
+function cloneRepository(cloneUrl, repoName) {
+    console.log(`Cloning ${repoName}...`);
+    execSync(`git clone ${cloneUrl}`, { stdio: 'inherit' });
+}
+
+async function cloneRepositories(username, token) {
+    const octokit = new Octokit({ auth: token });
+
+    try {
+        const { data: repos } = await octokit.repos.listForAuthenticatedUser({ username });
+        
+        repos.forEach((repo) => {
+            const cloneUrl = `https://${username}@github.com/${repo.full_name}.git`;
+            cloneRepository(cloneUrl, repo.full_name);
+        });
+
+        console.log("All repositories cloned successfully!");
+    } catch (error) {
+        console.error("Error fetching or cloning repositories:", error);
+    }
+}
+
+function main() {
+    const username = process.argv[2];
+    const token = process.argv[3];
+
+    if (!username || !token) {
+        console.error("Usage: node clone_repos.js <username> <token>");
+        process.exit(1);
+    }
+
+    cloneRepositories(username, token);
+}
+
+main();
